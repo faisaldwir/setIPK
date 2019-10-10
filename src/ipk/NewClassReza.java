04419f6974d55efdb774af8504c4946b16b1b8b1
DESKTOP-I1LJ9A0\Rezaditya <rezaditya28@gmail.com>
2019-10-10 10:59:50 +0700

    reza nambah class TambahClassCobaPull, variable & getter setter

---
+++ src/ipk/NewClassReza.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipk;
+
+/**
+ *
+ * @author Rezaditya
+ */
+public class NewClassReza {
+
+    /**
+     * @return the r
+     */
+    public int getR() {
+        return r;
+    }
+
+    /**
+     * @param r the r to set
+     */
+    public void setR(int r) {
+        this.r = r;
+    }
+    private int r;
+}
